92665d8ef47519e425f344e355b3a17b6ac7ceb8
Ekaterina Filonych <ekaterina.filonych@brotherhood.software>
2026-04-30 16:37:16 +0300

    feat: add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import * as process from 'node:process';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
   const PORT = process.env.PORT || 5001
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest-bcknd')
+    .setDescription("Nest bcknd project")
+    .setVersion("1.0.0")
+    .addTag('Study')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
 
 interface UserCreationAttrs {
   email: string;
@@ -7,18 +8,23 @@ interface UserCreationAttrs {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs>{
+  @ApiProperty({example: '1', description: 'User id'})
   @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
   declare id: number;
 
+  @ApiProperty({example: 'user@gmail.com', description: 'Email'})
   @Column({type: DataType.STRING, unique: true, allowNull: false})
   email: string;
 
+  @ApiProperty({example: '1234', description: 'Password'})
   @Column({type: DataType.STRING, allowNull: false})
   password: string;
 
+  @ApiProperty({example: 'true', description: 'Ban flag'})
   @Column({type: DataType.BOOLEAN, defaultValue: false})
   banned: boolean;
 
+  @ApiProperty({example: 'spam', description: 'Ban reason'})
   @Column({type: DataType.STRING, allowNull: true})
   banReason: string;
 }
